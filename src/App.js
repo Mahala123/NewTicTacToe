22845742a7c28017b8942d0c79403446e909efec
sambit <sambitmahala653@gmail.com>
2022-11-11 17:32:33 +0530

    add style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 //import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import Demo from './components/Demo';
+import './components/style/Style.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
     <h3>TIC TAC TOE</h3>
       <Demo/>
     </div>
